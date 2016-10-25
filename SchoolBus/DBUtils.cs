3d6749f580a2684e54ba278b37a0541a7fa13136
mousewip <mousewip@gmail.com>
2016-10-25 21:01:42 +0700

    Connect database using SQLServer

---
+++ SchoolBus/DBUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolBus
+{
+    class DBUtils
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string datasource = @"MOUSEWIP";
+
+            string database = "SchoolBus";
+            string username = "sa";
+            string password = "***";
+
+            return DBSQLServerUtils.GetDBConnection(datasource, database, username, password);
+        }
+    }
+}
--- SchoolBus/Program.cs
+++ SchoolBus/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace SchoolBus
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Getting Connection ...");
+            SqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+
             XuLy xl = new XuLy();
             xl.ReadData();
             xl.TotalTime = 2400;
